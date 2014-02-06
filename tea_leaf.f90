58f1d9b07a4993a5e567550ae462165da4893511
Wayne Gaudin <wpg@pan.hpsg.dcs.warwick.ac.uk>
2014-02-06 12:16:23 +0000

    more headers

--- tea_leaf.f90
+++ tea_leaf.f90
@@ -1,3 +1,39 @@
+!Crown Copyright 2014 AWE.
+!
+! This file is part of TeaLeaf.
+!
+! TeaLeaf is free software: you can redistribute it and/or modify it under
+! the terms of the GNU General Public License as published by the
+! Free Software Foundation, either version 3 of the License, or (at your option)
+! any later version.
+!
+! TeaLeaf is distributed in the hope that it will be useful, but
+! WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+! FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+! details.
+!
+! You should have received a copy of the GNU General Public License along with
+! TeaLeaf. If not, see http://www.gnu.org/licenses/.
+
+!>  @brief TeaLeaf top level program: Invokes the main cycle
+!>  @author David Beckingsale, Wayne Gaudin
+!>  @details TeaLeaf in a proxy-app that solves the linear heat conduction
+!>  equations using an implicit finite volume method on a Cartesian grid.
+!>  The grid is staggered with internal energy, density, temperature and pressure at cell
+!>  centres and velocities on cell vertices.
+!>
+!>  The CloverLeaf hydrodynamics can also be invoked to produce a coupled conduction/advection system
+!>
+!>  It can be run in distributed mode using MPI.
+!>
+!>  It can use OpenMP, OpenACC on a compute device.
+!>
+!>  NOTE: that the proxy-app uses uniformly spaced mesh. The actual method will
+!>  work on a mesh with varying spacing to keep it relevant to it's parent code.
+!>  For this reason, optimisations should only be carried out on the software
+!>  that do not change the underlying numerical method. For example, the
+!>  volume, though constant for all cells, should remain array and not be
+!>  converted to a scalar.
 PROGRAM tea_leaf
 
   USE clover_module
