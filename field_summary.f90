fb74b5116feb10d10003b01aef93788c2e14c979
Wayne Gaudin <wg14846@newblue2.cm.cluster>
2014-11-07 11:55:15 +0000

    removed debug print

--- field_summary.f90
+++ field_summary.f90
@@ -84,7 +84,6 @@ SUBROUTINE field_summary()
 !$    IF(OMP_GET_THREAD_NUM().EQ.0) THEN
         IF(test_problem.GE.1) THEN
   ! Note that the "correct" solution is with IEEE switched on, 1 task, 1 thread, Intel compiler on Ivy Bridge
-write(0,*)"temp ", temp
           IF(test_problem.EQ.1) qa_diff=ABS((100.0_8*(temp/157.550841832793_8))-100.0_8)
           IF(test_problem.EQ.2) qa_diff=ABS((100.0_8*(temp/116.067951160930_8))-100.0_8)
           IF(test_problem.EQ.3) qa_diff=ABS((100.0_8*(temp/95.4865103390698_8))-100.0_8)
