9f10f9d4293d6fcc2b34748df7fdfbdcb7c46c10
Wayne Gaudin <wg14846@newblue4.cm.cluster>
2014-11-07 12:18:33 +0000

    Last correct answer

--- field_summary.f90
+++ field_summary.f90
@@ -88,7 +88,7 @@ SUBROUTINE field_summary()
           IF(test_problem.EQ.2) qa_diff=ABS((100.0_8*(temp/116.067951160930_8))-100.0_8)
           IF(test_problem.EQ.3) qa_diff=ABS((100.0_8*(temp/95.4865103390698_8))-100.0_8)
           IF(test_problem.EQ.4) qa_diff=ABS((100.0_8*(temp/166.838315378708_8))-100.0_8)
-          IF(test_problem.EQ.5) qa_diff=ABS((100.0_8*(temp/95.4865103654282_8))-100.0_8)
+          IF(test_problem.EQ.5) qa_diff=ABS((100.0_8*(temp/116.482111627676_8))-100.0_8)
 
           WRITE(*,'(a,i4,a,e16.7,a)')"Test problem", Test_problem," is within",qa_diff,"% of the expected solution"
           WRITE(g_out,'(a,i4,a,e16.7,a)')"Test problem", Test_problem," is within",qa_diff,"% of the expected solution"
