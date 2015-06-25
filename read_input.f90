706b34b86d7281fd40ddb56a18eac98fc5c22102
michael boulton <mb8224@my.bristol.ac.uk>
2015-06-25 13:44:49 +0100

    only care about halo exchange depth + preconditioner with ppcg

--- read_input.f90
+++ read_input.f90
@@ -294,7 +294,7 @@ SUBROUTINE read_input()
     IF(parallel%boss)WRITE(g_out,"(1x,a25,i12)")'tl_ppcg_inner_steps',tl_ppcg_inner_steps
   endif
 
-  if ((halo_exchange_depth .gt. 1) .and. (tl_preconditioner_type .eq. TL_PREC_JAC_BLOCK)) then
+  if ((halo_exchange_depth .gt. 1) .and. (tl_preconditioner_type .eq. TL_PREC_JAC_BLOCK) .and. tl_use_ppcg) then
     call report_error('read_input', 'Unable to use nonstandard halo depth with block jacobi preconditioner')
   endif
 
