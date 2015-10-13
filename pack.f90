b8f263f497b30d80dbd8d2300202a2aee82f84eb
Michael Boulton <michael.boulton@bristol.ac.uk>
2015-10-13 11:47:40 +0100

    Only adding offset when packing bototm/top

--- pack.f90
+++ pack.f90
@@ -51,16 +51,15 @@ SUBROUTINE call_packing_functions(fields, depth, face, packing, mpi_buffer, offs
       tile_offset = (chunk%tiles(t)%bottom - chunk%bottom)*depth
     CASE (CHUNK_BOTTOM, CHUNK_TOP)
       tile_offset = (chunk%tiles(t)%left - chunk%left)*depth
+      IF (tile_offset .NE. 0) THEN
+        tile_offset = tile_offset + depth*depth
+      ENDIF
     CASE DEFAULT
       CALL report_error("pack.f90","Invalid face pased to buffer packing")
     END SELECT
 
     IF (chunk%tiles(t)%tile_neighbours(face) .NE. EXTERNAL_FACE) THEN
       CYCLE
-    ENDIF
-
-    IF (tile_offset .NE. 0) THEN
-      tile_offset = tile_offset + depth*depth
     ENDIF
 
     CALL pack_all(chunk%tiles(t)%field%x_min,                    &
