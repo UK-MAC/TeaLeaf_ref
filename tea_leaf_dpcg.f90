151cacfc46305a4086d4343dedf848b552f1c33d
rsmedleystevenson <richard.smedley-stevenson@awe.co.uk>
2015-11-15 21:03:00 +0000

    Added coarse_solve_eps tolerance and inner_cg and inner_ch arrays

--- tea_leaf_dpcg.f90
+++ tea_leaf_dpcg.f90
@@ -11,11 +11,9 @@ MODULE tea_leaf_dpcg_module
 
   IMPLICIT NONE
 
-  INTEGER, PARAMETER :: coarse_solve_max_iters=200
-
   LOGICAL :: inner_use_ppcg
-  REAL(KIND=8), DIMENSION(coarse_solve_max_iters) :: inner_cg_alphas, inner_cg_betas
-  REAL(KIND=8), DIMENSION(coarse_solve_max_iters) :: inner_ch_alphas, inner_ch_betas
+  REAL(KIND=8), DIMENSION(:), ALLOCATABLE :: inner_cg_alphas, inner_cg_betas
+  REAL(KIND=8), DIMENSION(:), ALLOCATABLE :: inner_ch_alphas, inner_ch_betas
   REAL(KIND=8) :: eigmin, eigmax, theta
 
 CONTAINS
@@ -31,6 +29,13 @@ SUBROUTINE tea_leaf_dpcg_init_x0(solve_time)
 
   ! done before
   !CALL tea_leaf_calc_residual()
+
+  IF (.NOT. ALLOCATED(inner_cg_alphas)) THEN
+    ALLOCATE(inner_cg_alphas(coarse_solve_max_iters))
+    ALLOCATE(inner_cg_betas (coarse_solve_max_iters))
+    ALLOCATE(inner_ch_alphas(coarse_solve_max_iters))
+    ALLOCATE(inner_ch_betas (coarse_solve_max_iters))
+  ENDIF
 
   CALL tea_leaf_dpcg_coarsen_matrix()
 
@@ -61,7 +66,7 @@ SUBROUTINE tea_leaf_dpcg_init_x0(solve_time)
       chunk%def%def_w,                               &
       chunk%def%def_z, &
       chunk%def%def_sd, &
-      eps, &
+      coarse_solve_eps, &
       coarse_solve_max_iters,                          &
       it_count,         &
       0.0_8,            &
@@ -190,7 +195,7 @@ SUBROUTINE tea_leaf_dpcg_setup_and_solve_E(solve_time)
       chunk%def%def_w,                               &
       chunk%def%def_z, &
       chunk%def%def_sd, &
-      eps, &
+      coarse_solve_eps, &
       coarse_solve_max_iters,                          &
       it_count,         &
       theta,            &
